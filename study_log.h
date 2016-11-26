2c5d237b10d66cfc4736f04185f0fad4d352fa0e
fjut <fjut@qq.com>
2016-11-26 22:54:02 +0800

    android.mk

--- study_log.h
+++ study_log.h
@@ -295,32 +295,27 @@ Scene* pScene = Director::getInstance()->getRunningScene();
 pScene->runAction(act);
 
 8. android.mk 包含文件： （ http://blog.ready4go.com/blog/2013/10/12/update-android-dot-mk-with-local-src-files-and-local-c-includes/ ）
-# 配置自己的源文件目录和源文件后缀名
-MY_FILES_PATH  :=  $(LOCAL_PATH) \
+# 配置自己的源文件目录
+MY_FILES_PATH  :=  $(LOCAL_PATH) \ #（为了寻找 jni/hellocpp/main.cpp）
 $(LOCAL_PATH)/../../Classes
-
+# 指定查找源文件后缀名
 MY_FILES_SUFFIX := %.cpp %.c
-
 # 递归遍历目录下的所有的文件
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
-
 # 获取相应的源文件
 MY_ALL_FILES := $(foreach src_path,$(MY_FILES_PATH), $(call rwildcard,$(src_path),*.*) )
 MY_ALL_FILES := $(MY_ALL_FILES:$(MY_CPP_PATH)/./%=$(MY_CPP_PATH)%)
 MY_SRC_LIST  := $(filter $(MY_FILES_SUFFIX),$(MY_ALL_FILES))
 MY_SRC_LIST  := $(MY_SRC_LIST:$(LOCAL_PATH)/%=%)
-
 # 去除字串的重复单词
 define uniq =
 $(eval seen :=)
 $(foreach _,$1,$(if $(filter $_,${seen}),,$(eval seen += $_)))
 ${seen}
 endef
-
 # 递归遍历获取所有目录
 MY_ALL_DIRS := $(dir $(foreach src_path,$(MY_FILES_PATH), $(call rwildcard,$(src_path),*/) ) )
 MY_ALL_DIRS := $(call uniq,$(MY_ALL_DIRS))
-
 # 赋值给NDK编译系统
 LOCAL_SRC_FILES  := $(MY_SRC_LIST)
 LOCAL_C_INCLUDES := $(MY_ALL_DIRS)
