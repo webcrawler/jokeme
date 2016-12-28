9d3c6579ff0dcfae124f66357e4df21ecfe84707
1274219974@qq.com <clfnh1023>
2016-12-28 11:52:12 +0800

    copy dir

--- SomeScripts/copy-mk-file.bat
+++ SomeScripts/copy-mk-file.bat
@@ -1,9 +1,11 @@
 
-set path=E:\MyWork\ScmjGameNew\Clicent\GameSC\cocos2d\external
+set path=E:\xxx\cocos2d\external
 set file=prebuilt\android\Android.mk
+set copyPath=prebuilt\android\armeabi
 
 for /d %%i in (*) do (
 copy /y %~dp0%%i\%file% %path%\%%i\%file%
+copy /y %~dp0%%i\%copyPath% %path%\%%i\%copyPath%
 )
 :: %~dp0 ->µ±Ç°Ä¿Â¼
 copy /y %~dp0joke.mk %path%
