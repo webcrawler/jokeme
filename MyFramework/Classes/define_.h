66b5a99aa4160cc8e7b3d4b2dafa4310452ca2b3
1274219974@qq.com <clfnh1023>
2017-01-17 10:44:37 +0800

    android

--- MyFramework/Classes/define_.h
+++ MyFramework/Classes/define_.h
@@ -6,6 +6,15 @@
 #include "Control/define_control.h"
 
 
+/*
+adb logcat> E:\MyGithub\jokeme\MyFramework\log10.txt
+ndk-stack -symE:\MyGithub\jokeme\MyFramework\proj.android\obj\local\ameabi -dump E:\MyGithub\jokeme\MyFramework\log10.txt
+
+
+E:
+cd E:\MyGithub\jokeme\MyFramework
+cocos run -p android --ap android-15 --ndk-toolchain arm-linux-androideabi-4.8
+*/
 
 
 #endif 
