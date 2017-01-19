9c237f76d9b325e59b891e109413f368c7db4044
1274219974@qq.com <clfnh1023>
2017-01-19 14:03:29 +0800

    MyFramework\Classes\Views\Test\Layer3DTest.cpp MyFramework\Classes\Views\Test\Layer3DTest.h

---
+++ MyFramework/Classes/Views/Test/Layer3DTest.cpp
@@ -0,0 +1,67 @@
+
+#include "Layer3DTest.h"
+
+Layer3DTest* Layer3DTest::create()
+{
+	Layer3DTest* ptr = new Layer3DTest();
+	if (ptr && ptr->init())
+	{
+		ptr->autorelease();
+		return ptr;
+	}
+	return nullptr;
+}
+
+string Layer3DTest::getWidgetFilePath()
+{
+	return "layer_test";
+}
+
+string Layer3DTest::getLayerName()
+{
+	return "UI_Layer3DTest";
+}
+
+void Layer3DTest::updateUI()
+{
+	mRoot->setVisible(false);
+
+	//auto orc = Sprite3D::create("Sprite3DTest/orc.c3b");
+	//orc->setScale(5);
+	//orc->setNormalizedPosition(Vec2(.5f, .3f));
+	//orc->setPositionZ(40);
+	//orc->setRotation3D(Vec3(0, 180, 0));
+	//orc->setGlobalZOrder(-1);
+	//addChild(orc);
+
+	//auto ship = Sprite3D::create("Sprite3DTest/boss1.obj");
+	//ship->setScale(5);
+	//ship->setTexture("Sprite3DTest/boss.png");
+	//ship->setNormalizedPosition(Vec2(.5, .5));
+	//ship->setRotation3D(Vec3(90, 0, 0));
+	//ship->setForceDepthWrite(true);
+	//addChild(ship);
+
+	//auto sprite = Sprite3D::create("Sprite3DTest/boss1.obj");
+	//sprite->setScale(3.f);
+	//sprite->setTexture("Sprite3DTest/boss.png");
+	//addChild(sprite);
+	//sprite->setPosition(Vec2(100, 100));
+	//ActionInterval* action;
+	//float random = CCRANDOM_0_1();
+
+	//if (random < 0.20)
+	//	action = ScaleBy::create(3, 2);
+	//else if (random < 0.40)
+	//	action = RotateBy::create(3, 360);
+	//else if (random < 0.60)
+	//	action = Blink::create(1, 3);
+	//else if (random < 0.8)
+	//	action = TintBy::create(2, 0, -255, -255);
+	//else
+	//	action = FadeOut::create(2);
+	//auto action_back = action->reverse();
+	//auto seq = Sequence::create(action, action_back, nullptr);
+	//sprite->runAction(RepeatForever::create(seq));
+
+}
